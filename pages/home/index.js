aa2b45bcaabd6c807a3df9c676686467e2f9a7b7
Ricardo do Vale <ricardovcza@gmail.com>
2022-10-27 13:58:26 -0300

    feat: add requisition, render cards by DOM, add event listeners

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,21 @@
-/* Desenvolva seu script aqui */
+import { renderNews } from "../../scripts/renderNews.js";
+import { fetchNews } from "../../scripts/requisitions.js";
+let currentPage = 0
+let nextPage = null
+
+const observer = new IntersectionObserver(async entries => {
+    if(entries[0].isIntersecting) {
+        renderCurrentPage()
+    }
+})
+const footerElement = document.querySelector('.bg-footer')
+observer.observe(footerElement)
+
+async function renderCurrentPage() {
+    const newsArray = await fetchNews(currentPage)
+    let nextPage = newsArray.nextPage
+    if(nextPage > currentPage) {
+        currentPage++
+        return renderNews(newsArray)
+    }
+}
